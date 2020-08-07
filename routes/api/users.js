5d248484367d3fc48bcb62ee4210ca7c780d69da
eriktheredhead <erikhough03@gmail.com>
2020-08-06 19:05:18 -0600

    backend finished, working through front end

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,7 +74,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: 36000000 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
