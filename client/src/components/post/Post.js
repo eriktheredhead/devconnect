07bc1df3c15d44fb4883f57fef3470b055a22744
eriktheredhead <erikhough03@gmail.com>
2020-08-10 18:50:00 -0600

    prepared for deployment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "../post/CommentForm";
+import CommentItem from "../post/CommentItem";
 import { getPost } from "../../actions/post";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost]);
+  }, [getPost, match.params.id]);
 
   return loading || post === null ? (
     <Spinner />
@@ -19,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map(comment => (
+          <CommentItem key={comment.id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
